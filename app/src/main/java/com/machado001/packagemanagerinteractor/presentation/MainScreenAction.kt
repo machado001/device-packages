4a08b637694d7bddfb0f8f5b688281c65a909461
machado001 <machadowg@gmail.com>
2025-01-11 18:14:49 -0300

    delete unused files

--- app/src/main/java/com/machado001/packagemanagerinteractor/presentation/MainScreenAction.kt
+++
@@ -1,6 +0,0 @@
-package com.machado001.packagemanagerinteractor.presentation
-
-sealed interface MainScreenAction {
-    data class OnUserSearch(val query: String) : MainScreenAction
-    data object OnUserNavigateToPackageDetails : MainScreenAction
-}
--- app/src/main/java/com/machado001/packagemanagerinteractor/presentation/MainScreenEvent.kt
+++
@@ -1,8 +0,0 @@
-package com.machado001.packagemanagerinteractor.presentation
-
-sealed interface MainScreenEvent {
-    data class Error(val error: UiError) : MainScreenEvent
-    data object OnLoadData : MainScreenEvent
-}
-
-class UiError
